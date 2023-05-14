e8b3f0dcd44e7bf2f116116182b9eb23a84c63e9
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-14 16:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let isPalindrome = true;
+	let i = 0, j = str.length-1;
+	while(i <= j && isPalindrome){
+		if(str[i] != str[j]){
+			isPalindrome = false;
+		}
+		i++;
+		j--;
+	}
+	console.log(isPalindrome ? "Palindrome : Not a Palindrome")
 }
 module.exports = palindrome
